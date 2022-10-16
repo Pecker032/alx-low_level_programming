8fbe0509c99adcd5aed6ec4e2f96279b5b43b2f3
pecker032 <promiseokamudu998@gmail.com>
2022-10-16 01:50:00 -0700

    ff

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
- *  *main- Serves as the entry point for the program
+ *  * main-entry point for the program.
  *   *
- *    *Return: By default returns zero if no erros otherwise returns non-zero value.
+ *    * Return:0 if no errors, return non zero if errors
  *     */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}}
