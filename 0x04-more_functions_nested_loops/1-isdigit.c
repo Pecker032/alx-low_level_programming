2229a9609c2f261c6cc8eb2d78eefa9dd0fbcd30
pecker032 <promiseokamudu998@gmail.com>
2022-10-21 12:20:49 -0700

    rea

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,14 +5,14 @@
  * @c: input character.
  * Return: 1 if is an uppercase character, 0 in other case.
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+		if (c >= 48 && c <= 57)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
 }
